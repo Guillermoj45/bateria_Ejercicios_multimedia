fa29630e2b48ae10ffb4d8c866f8ed4d6601649b
Guillermo <gremesalgarcia@safareyes.es>
2024-10-19 22:42:37 +0200

    Actividad 3 hecha

--- app/src/main/java/com/example/ejercicio3/TercerLayout.kt
+++ app/src/main/java/com/example/ejercicio3/TercerLayout.kt
@@ -1,6 +1,9 @@
 package com.example.ejercicio3
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +20,12 @@ class TercerLayout : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val handle = Handler(Looper.getMainLooper())
+        handle.postDelayed({
+            Intent(this, Ejercicio3::class.java)
+        },
+            5000
+        )
     }
 }
